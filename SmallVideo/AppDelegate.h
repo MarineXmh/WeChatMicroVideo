8830897fc0d8b8d87db42a6f1964b38225962702
MarineXmh <15957299a@gmail.com>
2015-12-04 16:48:48 +0800

    Initial Commit

---
+++ SmallVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmallVideo
+//
+//  Created by Xu Menghua on 15/12/4.
+//  Copyright © 2015年 Xu Menghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmallVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmallVideo
+//
+//  Created by Xu Menghua on 15/12/4.
+//  Copyright © 2015年 Xu Menghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
